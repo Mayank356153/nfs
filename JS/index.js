ad842b1256ed8a3c8a75109285d12cd5c12ce00b
HarshSingh011 <141523149+HarshSingh011@users.noreply.github.com>
2024-10-15 12:46:13 +0530

    styling start page

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,6 @@
-
-localStorage.setItem("level",1);
-localStorage.setItem("bossSpeed",0.3);
-var level = localStorage.getItem("level");
-var acceleration = localStorage.getItem("bossSpeed");
-console.log(level,acceleration);
-localStorage.setItem("check",true);
-console.log()
-document.addEventListener('keypress',function (event){
-    if(event.key=='Enter')
-    window.location.href ="../HTML/start.html";
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        window.location.href = "../HTML/start.html"; // Redirect to start page
+    }
 });
 
